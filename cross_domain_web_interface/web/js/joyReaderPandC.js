363abd6d41f5b706df1172a0ae0f5b262700182e
Trevor Sherrard <sherrardtr@gmail.com>
2021-06-09 18:59:08 +0000

    fix broken endpoint

--- cross_domain_web_interface/web/js/joyReaderPandC.js
+++ cross_domain_web_interface/web/js/joyReaderPandC.js
@@ -6,41 +6,40 @@
 
 // read fwdRev data joystick and send data to flask app on 200 ms interval
 var Joy4 = new JoyStick('joy4');
-var frURL = "http://localhost:5000/fwdRevJoyPost";
-var frData = {
-	"FwdRev": 0,
-};
 
-// read spin data joystick and send data to flask app on 200 ms interval
-var spinURL = "http://localhost:5000/spinJoyPost"
-var spinData = {
-	"spin": 0
+var fwdRevURL = "http://robotcontrol.live:5000/setFwdRev";
+var spinURL = "http://robotcontrol.live:5000/setSpin";
+var fwdRevData = {
+        "fwdRev": 0
 }
 
-function sendFRData()
+var spinData = {
+        "spin": 0
+}
+
+function sendFwdRevData()
 {
-	$.ajax({type: 'POST',
-		url: frURL,
-		data: JSON.stringify (frData),
-		success: function(data) {  },
-		contentType: "application/json",
-		dataType: 'json'
-	});
+        $.ajax({type: 'POST',
+                url: fwdRevURL,
+                data: JSON.stringify (fwdRevData),
+                success: function(data) {  },
+                contentType: "application/json",
+                dataType: 'json'
+        });
 }
 
 function sendSpinData()
 {
-	$.ajax({type: 'POST',
-		url: spinURL,
-		data: JSON.stringify (spinData),
-		success: function(data) {  },
-		contentType: "application/json",
-		dataType: 'json'
-	});
+        $.ajax({type: 'POST',
+                url: spinURL,
+                data: JSON.stringify (spinData),
+                success: function(data) {  },
+                contentType: "application/json",
+                dataType: 'json'
+        });
 }
 
-setInterval(function(){ frData.FwdRev=Joy4.GetY(); }, 50);
-setInterval(function(){ sendFRData() }, 200);
-setInterval(function(){ spinData.spin=Joy4.GetX(); }, 50);
-setInterval(function(){ sendSpinData() }, 200);
-
+setInterval(function(){ spinData.spin = Joy4.GetX(); }, 300);
+setInterval(function(){ fwdRevData.fwdRev = Joy4.GetY(); }, 300);
+setInterval(function(){ sendFwdRevData() }, 500);
+setInterval(function(){ sendSpinData() }, 500);
