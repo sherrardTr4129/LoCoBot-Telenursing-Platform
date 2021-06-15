7adec96c0649cf6a980ac0d57a5680c44f381523
Trevor Sherrard <sherrardtr@gmail.com>
2021-06-15 22:42:08 +0000

    update javascript manager for ADAS-like interface

--- cross_domain_web_interface/web/js/divColorControl.js
+++ cross_domain_web_interface/web/js/divColorControl.js
@@ -6,9 +6,30 @@
 
 function set_div_color(div_id, color)
 {
-	var elem_id = "d_" + i;
+	var elem_id = "d_" + div_id;
         var element_ref = document.getElementById(elem_id);
-        element_ref.style.backgroundColor = "yellow";
+        element_ref.style.backgroundColor = color;
 }
 
-cycle_divs();
+function set_colors()
+{
+	const url = "http://robotcontrol.live:5000/getLiDARdata";
+	$.ajax({type: 'GET',
+                url: url,
+                success: function(data) {
+			for(var i = 0; i < data.length; i++)
+			{
+				var color;
+				if(data[i] == 0){color = "white";}
+				else if(data[i] == 1){color = "yellow";}
+				else if(data[i] == 2){color = "red";}
+				set_div_color(i + 1, color);
+				console.log(i, color, data[i]);
+			}
+		},
+                contentType: "application/json",
+                dataType: 'json'
+        });
+}
+
+setInterval(function(){ set_colors() }, 500);
